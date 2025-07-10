9b9866d96e869a5d0ff56a1aa30196ece653a5c5
bakarskaraya@yahoo.com <bakarskaraya@yahoo.com>
2025-07-10 21:19:33 +0300

    update app modult to serve index.html

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,34 @@
-import { Module }           from '@nestjs/common';
-import { PricesModule }     from './prices/prices.module';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import * as express from 'express';
+import { PricesModule } from './prices/prices.module';
 import { ProfitController } from './profit/profit.controller';
 import { ProfitService }    from './profit/profit.service';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join }              from 'path';
 
 @Module({
   imports: [
+    // Serve static assets from /public
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
+      exclude: ['/api*'],
     }),
     PricesModule,
   ],
   controllers: [ProfitController],
-  providers:    [ProfitService],
+  providers:   [ProfitService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    // Any non-API route, serve index.html
+    consumer
+      .apply((_req, res, next) => {
+        if (!_req.originalUrl.startsWith('/api')) {
+          res.sendFile(join(__dirname, '..', 'public', 'index.html'));
+        } else {
+          next();
+        }
+      })
+      .forRoutes('*');
+  }
+}
