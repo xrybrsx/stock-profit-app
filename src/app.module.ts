97444fe2faceaeb09363f0bd58ff921085cd1256
bakarskaraya@yahoo.com <bakarskaraya@yahoo.com>
2025-07-11 13:24:49 +0300

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,15 @@
 import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
-import * as express from 'express';
-import { PricesModule } from './prices/prices.module';
-import { ProfitController } from './profit/profit.controller';
-import { ProfitService }    from './profit/profit.service';
+import { ServeStaticModule }                   from '@nestjs/serve-static';
+import { join }                                from 'path';
+import { PricesModule }                        from './prices/prices.module';
+import { ProfitController }                    from './profit/profit.controller';
+import { ProfitService }                       from './profit/profit.service';
 
 @Module({
   imports: [
-    // Serve static assets from /public
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
-      exclude: ['/api*'],
+      exclude: ['/api*'],   // leave /api to your controllers
     }),
     PricesModule,
   ],
@@ -20,14 +18,23 @@ import { ProfitService }    from './profit/profit.service';
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    // Any non-API route, serve index.html
     consumer
-      .apply((_req, res, next) => {
-        if (!_req.originalUrl.startsWith('/api')) {
-          res.sendFile(join(__dirname, '..', 'public', 'index.html'));
-        } else {
-          next();
+      .apply((req, res, next) => {
+        const url = req.originalUrl;
+
+        // 1) Ignore API calls
+        if (url.startsWith('/api')) {
+          return next();
         }
+
+        // 2) Ignore any request for a "file" (has an extension)
+        //    so that .js/.css/.png etc are served normally
+        if (url.includes('.') ) {
+          return next();
+        }
+
+        // 3) Otherwise, serve index.html
+        res.sendFile(join(__dirname, '..', 'public', 'index.html'));
       })
       .forRoutes('*');
   }
